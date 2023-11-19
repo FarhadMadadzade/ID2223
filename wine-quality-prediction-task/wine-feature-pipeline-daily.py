b65d2f4fc1cc25aa9944142cb973d9c6bbcd59b8
Farhad <farhadm1999@gmail.com>
2023-11-19 17:11:09 +0100

    fixed daily feature pipeline and training pipeline works good

--- wine-quality-prediction-task/wine-feature-pipeline-daily.py
+++ wine-quality-prediction-task/wine-feature-pipeline-daily.py
@@ -13,11 +13,27 @@ def main():
     # hopsworks_project = hopsworks.login()
     fs = hopsworks_project.get_feature_store()
 
-    wine_features = fs.get_feature_group(name="wine_features", version=1)
+    random_quality = random.randint(1, 4)
+    wine_feature_per_quality = fs.get_feature_group(
+        name=f"wine_features_quality_{random_quality}", version=1
+    )
 
-    print(wine_features.features)
+    wine_features_statistics = wine_feature_per_quality.get_statistics()
 
-    quality = random.randint(1, 4)
+    new_wine_features = {}
+    for stat in wine_features_statistics.content["columns"]:
+        new_wine_features[stat["column"]] = [
+            max(0, random.gauss(stat["mean"], stat["stdDev"]))
+        ]
+        if stat["column"] == "wine_type":
+            new_wine_features[stat["column"]][0] = round(
+                new_wine_features[stat["column"]][0]
+            )
+    new_wine_features["quality"] = [random_quality]
+    new_wine_features = pd.DataFrame(new_wine_features)
+
+    main_features = fs.get_feature_group(name="wine_features", version=1)
+    main_features.insert(new_wine_features)
 
 
 if __name__ == "__main__":
