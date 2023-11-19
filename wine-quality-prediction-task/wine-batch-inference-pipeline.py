3d49da5c1c88ca589c4e3321bb1ba90f6f55343d
Calin Petrea <calinp@kth.ug.se>
2023-11-19 21:13:01 +0100

    fix daily inference pipeline for wine

--- wine-quality-prediction-task/wine-batch-inference-pipeline.py
+++ wine-quality-prediction-task/wine-batch-inference-pipeline.py
@@ -1,104 +1,85 @@
 import os
-import modal
-    
-LOCAL=True
+import pandas as pd
+import hopsworks
+import joblib
+import datetime
+from PIL import Image
+from datetime import datetime
+import dataframe_image as dfi
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot
+import seaborn as sns
+import requests
+import random
 
-if LOCAL == False:
-   stub = modal.Stub()
-   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks","joblib","seaborn","sklearn==1.1.1","dataframe-image"])
-   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
-   def f():
-       g()
+project = hopsworks.login()
+fs = project.get_feature_store()
 
-def g():
-    import pandas as pd
-    import hopsworks
-    import joblib
-    import datetime
-    from PIL import Image
-    from datetime import datetime
-    import dataframe_image as dfi
-    from sklearn.metrics import confusion_matrix
-    from matplotlib import pyplot
-    import seaborn as sns
-    import requests
-    import random
+mr = project.get_model_registry()
+model = mr.get_model("wine_model", version=5)
+model_dir = model.download()
+model = joblib.load(model_dir + "/wine_model.pkl")
 
-    project = hopsworks.login()
-    fs = project.get_feature_store()
-    
-    mr = project.get_model_registry()
-    model = mr.get_model("wine_model", version=5)
-    model_dir = model.download()
-    model = joblib.load(model_dir + "/wine_model.pkl")
-    
-    feature_view = fs.get_feature_view(name="wine_features", version=1)
-    batch_data = feature_view.get_batch_data()
-    batch_data = batch_data.drop('wine_type', axis=1)
+feature_view = fs.get_feature_view(name="wine_features", version=1)
+batch_data = feature_view.get_batch_data()
+batch_data = batch_data.drop('wine_type', axis=1)
 
-    y_pred = model.predict(batch_data)
-    #print(y_pred)
-    offset = random.randint(1, y_pred.size) 
-    print("Offset: " + str(offset))
-    wine = y_pred[y_pred.size-offset]
-    print("Wine quality predicted: " + str(wine))        
-    dataset_api = project.get_dataset_api()    
-   
-    wine_fg = fs.get_feature_group(name="wine_features", version=1)
-    df = wine_fg.read() 
-    #print(df)
-    label = df.iloc[-offset]["quality"]
-    print("Wine quality actual: " + str(label))
-    
-    monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
-                                                version=1,
-                                                primary_key=["datetime"],
-                                                description="Wine quality Prediction/Outcome Monitoring"
-                                                )
-    
-    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    data = {
-        'prediction': [str(wine)],
-        'label': [str(label)],
-        'datetime': [now],
-       }
-    monitor_df = pd.DataFrame(data)
-    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
-    
-    history_df = monitor_fg.read()
-    # Add our prediction to the history, as the history_df won't have it - 
-    # the insertion was done asynchronously, so it will take ~1 min to land on App
-    history_df = pd.concat([history_df, monitor_df])
+y_pred = model.predict(batch_data)
+#print(y_pred)
+offset = random.randint(1, y_pred.size) 
+print("Offset: " + str(offset))
+wine_pred = y_pred[y_pred.size-offset]
+print("Wine quality predicted: " + str(wine_pred))        
+dataset_api = project.get_dataset_api()    
+
+wine_fg = fs.get_feature_group(name="wine_features", version=1)
+df = wine_pred.read() 
+#print(df)
+label = df.iloc[-offset]["quality"]
+print("Wine quality actual: " + str(label))
+
+monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
+                                            version=1,
+                                            primary_key=["datetime"],
+                                            description="Wine quality Prediction/Outcome Monitoring"
+                                            )
+
+now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+data = {
+    'prediction': [str(wine_pred)],
+    'label': [str(label)],
+    'datetime': [now],
+    }
+monitor_df = pd.DataFrame(data)
+monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+
+history_df = monitor_fg.read()
+# Add our prediction to the history, as the history_df won't have it - 
+# the insertion was done asynchronously, so it will take ~1 min to land on App
+history_df = pd.concat([history_df, monitor_df])
 
 
-    df_recent = history_df.tail(4)
-    dfi.export(df_recent, './df_wine_recent.png', table_conversion = 'matplotlib')
-    dataset_api.upload("./df_wine_recent.png", "Resources/images", overwrite=True)
-    
-    predictions = history_df[['prediction']]
-    labels = history_df[['label']]
+df_recent = history_df.tail(4)
+dfi.export(df_recent, './df_wine_recent.png', table_conversion = 'matplotlib')
+dataset_api.upload("./df_wine_recent.png", "Resources/images", overwrite=True)
 
-    # Only create the confusion matrix when our wine_predictions feature group has examples of all 3 wine qualities
-    print("Number of different wine quality predictions to date: " + str(predictions.value_counts().count()))
-    if predictions.value_counts().count() == 3:
-        results = confusion_matrix(labels, predictions)
-    
-        df_cm = pd.DataFrame(results, ['True 1', 'True 2', 'True 3'], 
-                     ['Pred 1', 'Pred 2', 'Pred 3']) 
-    
-        cm = sns.heatmap(df_cm, annot=True)
-        fig = cm.get_figure()
-        fig.savefig("./confusion_wine_matrix.png")
-        dataset_api.upload("./confusion_wine_matrix.png", "Resources/images", overwrite=True)
-    else:
-        print("You need 3 different quality predictions to create the confusion matrix.")
-        print("Run the batch inference pipeline more times until you get 3 different quality wines") 
+predictions = history_df[['prediction']]
+labels = history_df[['label']]
 
+# Only create the confusion matrix when our wine_predictions feature group has examples of all 3 wine qualities
+print("Number of different wine quality predictions to date: " + str(predictions.value_counts().count()))
+if predictions.value_counts().count() == 3:
+    results = confusion_matrix(labels, predictions)
+    visualize_dfs(results)
 
-if __name__ == "__main__":
-    if LOCAL == True :
-        g()
-    else:
-        with stub.run():
-            f()
+    df_cm = pd.DataFrame(results, ['True 1', 'True 2', 'True 3'], 
+                    ['Pred 1', 'Pred 2', 'Pred 3']) 
+
+    cm = sns.heatmap(df_cm, annot=True)
+    fig = cm.get_figure()
+    fig.savefig("./confusion_wine_matrix.png")
+    dataset_api.upload("./confusion_wine_matrix.png", "Resources/images", overwrite=True)
+else:
+    print("You need 3 different quality predictions to create the confusion matrix.")
+    print("Run the batch inference pipeline more times until you get 3 different quality wines") 
 
