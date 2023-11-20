52c9466011a8352e58a0ceb0c953d010c2bef15a
Farhad <farhadm1999@gmail.com>
2023-11-20 21:21:05 +0100

    updated monitor

--- wine-quality-prediction-task/huggingface-spaces-wine-monitor/app.py
+++ wine-quality-prediction-task/huggingface-spaces-wine-monitor/app.py
@@ -14,18 +14,23 @@ dataset_api.download("Resources/images/confusion_wine_matrix.png")
 
 with gr.Blocks() as demo:
     with gr.Row():
-      with gr.Column():
-          gr.Label("Today's Predicted Image")
-          input_img = gr.Image("latest_wine.png", elem_id="predicted-img")
-      with gr.Column():          
-          gr.Label("Today's Actual Image")
-          input_img = gr.Image("actual_wine.png", elem_id="actual-img")        
+        with gr.Column():
+            gr.Label("Quality scores. Scores are given on a scale of 1-3 glasses.")
     with gr.Row():
-      with gr.Column():
-          gr.Label("Recent Prediction History")
-          input_img = gr.Image("df_wine_recent.png", elem_id="recent-predictions")
-      with gr.Column():          
-          gr.Label("Confusion Maxtrix with Historical Prediction Performance")
-          input_img = gr.Image("confusion_wine_matrix.png", elem_id="confusion-matrix")        
+        with gr.Column():
+            gr.Label("Today's Predicted Quality (1-3)")
+            input_img = gr.Image("latest_wine.png", elem_id="predicted-img")
+        with gr.Column():
+            gr.Label("Today's Actual Quality (1-3)")
+            input_img = gr.Image("actual_wine.png", elem_id="actual-img")
+    with gr.Row():
+        with gr.Column():
+            gr.Label("Recent Prediction History")
+            input_img = gr.Image("df_wine_recent.png", elem_id="recent-predictions")
+        with gr.Column():
+            gr.Label("Confusion Maxtrix with Historical Prediction Performance")
+            input_img = gr.Image(
+                "confusion_wine_matrix.png", elem_id="confusion-matrix"
+            )
 
 demo.launch()
